c0127940f4d583d82b41df2def43bf962b5d560d
diolclaudio <diolclaudio21@gmail.com>
2021-03-05 20:41:13 +0200

    first commit

---
+++ pays/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'pays'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('pagamentos/', views.pay, name='pay'),
+    path('confirmar/<int:id>', views.confirmar, name='confirmar'),
+    path('info/', views.info, name='info'),
+    path('comentario/', views.comentario, name='comentario'),
+    path('listas/', views.listas, name='listas'),
+ ]
